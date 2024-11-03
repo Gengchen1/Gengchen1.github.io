03f0829c3736d7e48f8cb1128dab3afc6580e904
Gengchen1 <zhaogengchen1@qq.com>
2024-11-03 22:04:12 +0300

    添加Shiki配置

--- astro.config.js
+++ astro.config.js
@@ -22,7 +22,37 @@ export default defineConfig({
   site: 'https://Gengchen1.github.io',
   integrations: [tailwind(), react(), sitemap()],
   markdown: {
-    syntaxHighlight: 'shiki', // 使用 Shiki 作为语法高亮引擎
+     shikiConfig: {
+      // 选择 Shiki 内置的主题（或添加你自己的主题）
+      // https://shiki.style/themes
+      theme: 'dracula',
+      // 另外，也提供了多种主题
+      // 查看下面关于使用亮/暗双主题的的说明
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      // 禁用默认配色
+      // https://shiki.style/guide/dual-themes#without-default-color
+      // (添加于 v4.12.0)
+      defaultColor: false,
+      // 添加自定义语言
+      // 注意：Shiki 内置了无数语言，包括 .astro！
+      // https://shiki.style/languages
+      langs: [],
+      // 为语言添加自定义别名
+      // 将别名映射到 Shiki 语言 ID：https://shiki.style/languages#bundled-languages
+      // https://shiki.style/guide/load-lang#custom-language-aliases
+      langAlias: {
+        cjs: "javascript"
+      },
+      // 启用自动换行，以防止水平滚动
+      wrap: true,
+      // 添加自定义转换器：https://shiki.style/guide/transformers
+      // 查找常用转换器：https://shiki.style/packages/transformers
+      transformers: [],
+    },
+    syntaxHighlight: true, // 使用 Shiki 作为语法高亮引擎
     smartypants: true,
     remarkPlugins: [remarkMath, remarkDirective, remarkEmbed, remarkSpoiler, remarkReadingTime],
     rehypePlugins: [
