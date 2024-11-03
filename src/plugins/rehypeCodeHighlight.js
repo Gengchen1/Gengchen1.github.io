0db47540b424e1afac24483b7b6915521703860a
Gengchen1 <zhaogengchen1@qq.com>
2024-11-03 22:34:29 +0300

    修改Highlight.css

--- src/plugins/rehypeCodeHighlight.js
+++ src/plugins/rehypeCodeHighlight.js
@@ -1,12 +1,11 @@
+import { getHighlighter } from 'shiki'
 import rehypeShiki from '@shikijs/rehype'
 
+const highlighter = await getHighlighter({
+  themes: ['github-light', 'github-dark'],
+  langs: ['javascript', 'python', 'c', 'C++'] // 添加你需要的语言
+})
+
 export const rehypeCodeHighlight = [
-  rehypeShiki,
-  {
-    themes: {
-      light: 'github-light',
-      dark: 'github-dark',
-    },
-    defaultColor: false,
-  },
+  [rehypeShiki, { highlighter }]
 ]
